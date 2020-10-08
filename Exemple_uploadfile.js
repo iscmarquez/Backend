b44dc6ee816654683c339141bee2691ea8014495
iscmarquez <38671799+iscmarquez@users.noreply.github.com>
2020-10-08 08:19:39 -0400

    Add files via upload

---
+++ Exemple_uploadfile.js
@@ -0,0 +1,27 @@
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.uploadDir='tmp_upload'; 
+  if (req.url == '/fileupload') {    
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = './uploaded/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {    // rename can move the file
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080); 
