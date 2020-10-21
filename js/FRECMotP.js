6c2fbd3ad319e9a11569edd3f723eebf0d4c15ec
pawlosierra <pawlosierra@hotmail.com>
2020-10-21 08:40:22 -0400

    Key recovery OK

--- js/FRECMotP.js
+++ js/FRECMotP.js
@@ -28,56 +28,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
-//Creamos el objeto de transporte
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'tpsoutienutilisateurs@gmail.com',
-    pass: 'Adminroot'
-  }
-});
-
-// var mensaje = "Hola desde nodejs...";
-
-// var mailOptions = {
-//   from: 'tpsoutienutilisateurs@gmail.com',
-//   to: 'pawlosierra1@gmail.com',
-//   subject: 'Recuperacion clave',
-//   text: mensaje
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email enviado: ' + info.response);
-//   }
-// });
-
 app.post('/User', (req, res, next) => {
-  // console.log(typeof res.res);
-  // console.log(res.body);
-  //res.send('received');
   const emailId = req.body;
   console.log(emailId);
-  // for (var key in emailId){
-  //   temp = key;
-  //   console.log(temp);
-  // }
-  // console.log("lo que necesito = ", temp);
-  // console.log(typeof temp);
-  // temp = emailId.toString();
-  // console.log(temp);
   try{
-  conexion.query(`select User.email, User.password from User where User.username = '${emailId.username}' and User.email = '${emailId.email}'`, (error, results, fields) => {
-    // conexion.query(`select User.username, User.email from User`, (error, results, fields) => {
-    // conexion.query(`select * from User`, (error, results, fields) => {
+  conexion.query(`select user.email, user.password from user where user.username = '${emailId.username}' and user.email = '${emailId.email}'`, (error, results, fields) => {
       console.log(JSON.stringify(results));
-      // if(error){
-        // if((error)||(email == undefined)){
         if(error){
-        console.log("yo estoy aqui");
         throw error;
         return res.json({ 
             success: false,
@@ -85,63 +42,60 @@ app.post('/User', (req, res, next) => {
         });
       }
       else{
-
-        //console.log(emailId.username);
-        console.log(results);
-        // let juan = Object.values(results);
-        // console.log(juan[0]);
-        // pawlo = Object.values(juan[0]);
-        // console.log(typeof pawlo);
-
-        // let r = new Map(Object.entries(results));
-        // console.log(r.has(emailId.username));
-        //for(let values of results.values()){
-      
-          // if(values == emailId.username){
-          //   console.log("ok");
-          // }
-       // }
-        // console.log(results)
-        // console.log(Object.values(results));
-        // for(key in results){
-        //   console.log(get(key));
-        // }
-        // console.log(results.indexOf(emailId.username))
-        //console.log(Object.keys(results));
-
-//////////////////
-
+          if(Object.entries(results).length===0){
+          return res.json({ 
+              success: false,
+              message: "Erreur : L'inscription n'a pas été insérée dans la base de données"
+            });
+        }
+        else{
         var emailClient = results[0].email;
-        console.log(typeof emailClient);
         var passwordClient = results[0].password;
-        console.log(typeof passwordClient);
 
-
-        var mailOptions = {
-          from: 'tpsoutienutilisateurs@gmail.com',
-          to: emailClient,
-          subject: 'Récupération du mot de passe',
-          text: 'Votre mot de passe est: '+passwordClient
-        };
-          transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-          } else {
-            console.log('e-mail envoyé: ' + info.response);
+        conexion.query(`select * from nodemailer`, (error, results, fields) => {
+          if(error){
+            throw error;
+            return res.json({ 
+                success: false,
+                message: "Erreur : L'inscription n'a pas été insérée dans la base de données"
+            });
           }
-        });
-        
-        return res.json({        
-          success: true,
-          message: "Le succès : Le mot de passe a été envoyée avec succès à l'adresse électronique enregistrée."
-      });
-
-      
+          else{
+              var service = results[0].service;
+              var user = results[0].user;
+              var pass = results[0].pass;
+              var from = results[0].from;
+              var subject= results[0].subject;
+              var text = results[0].text;
+              // nous créons l'objet de transport
+              var transporter = nodemailer.createTransport({
+                service: service,
+                auth: {
+                  user: user, 
+                  pass: pass, 
+                }
+              });
+              var mailOptions = {
+                from: from, 
+                to: emailClient,
+                subject: subject, 
+                text: text + passwordClient
+              };
+                transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('e-mail envoyé: ' + info.response);
+                }
+              });       
+              return res.json({        
+                success: true,
+                message: "Le succès : Le mot de passe a été envoyée avec succès à l'adresse électronique enregistrée."
+            });
+          }
+    });
     }
-  
-      //     throw error;
-      // return res.json(results);
-    
+    }
   });
 }
 catch(Err){
@@ -153,24 +107,6 @@ catch(Err){
       "statusText":Err.sqlMessage
       }
     );
-
-}
-  //throw 'myException';
-  
+} 
 });
-
-
-// app.get('/User/:emailId', (req, res, next) => {
-//     //  console.log(req.body);
-//     // res.send('received');
-//     const emailId = req.params.emailId;
-//     console.log(emailId);
-//     conexion.query(`select User.password, User.email from User where User.email = '${emailId}'`, (error, results, fields) => {
-//         if(error)
-//             throw error;
-//         return res.json(results);
-//     });
-// });
-
-
 app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
